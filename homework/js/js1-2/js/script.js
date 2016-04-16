ff31a3f3edad939ec8cc7de90284c5ef0914953e
TolikSyedin <toliksyedin@gmail.com>
2016-04-16 17:10:37 +0300

    final ver

--- homework/js/js1-2/js/script.js
+++ homework/js/js1-2/js/script.js
@@ -4,11 +4,11 @@ function pow (a, b) {
 	console.log('result = ', result);
 }
 
+
 pow(prompt('Enter base number'), prompt('Enter exponent number') );
 
 
 var names  = [];
-
 
 for (var i = 0; i < 5; i++) {
 	names.push(prompt('enter your name'));
@@ -17,21 +17,21 @@ for (var i = 0; i < 5; i++) {
 
 
 var userName = prompt('Enter username please');
-console.log('userName', userName)
 
 
 var rightName = false;
 
+
 for (var i = 0; i < names.length; i++) {
 
 	var nameItem = names[i];
-	console.log('nameItem = ', nameItem);
 
 	if (nameItem == userName) {
 		var rightName = true;
 		alert('Congratulations ' + userName + '! Login succesfull')
 		break
-	}
+	} 
+
 }
 
 
