67eac87bc8fa45fe974a1686f855b7dbac0b0091
TolikSyedin <toliksyedin@gmail.com>
2016-06-14 20:56:31 +0300

    update script.js

--- homework/js/hw15-16/js/script.js
+++ homework/js/hw15-16/js/script.js
@@ -23,4 +23,77 @@ $.ajax({
 
 	}
 });
+// window.pixabayCallback = function(){
+//     console.log('callback did it!')
+// }
 
+
+// $.ajax({
+//     url: 'https://pixabay.com/api/',
+//     data: {
+//         key: '2752016-4fdc55509940bb2ec126f7894',
+//         q: 'test',
+//         callback: pixabayCallback(),
+//     },
+//     method: 'GET',
+//     dataType: 'jsonp',
+//     success: function(data){
+//         console.log(data);
+//         var hits = data.hits;
+//         console.log(hits)
+//         for(var i = 0; i < hits.length; i++){
+            
+//         }
+//         return data;
+
+//         }
+// })
+
+// function Human() {
+//     this.name = 'humanName';
+//     this.age = 24;
+//     this.male = 'male';
+//     this.weight = 80;
+//     this.tall = 180;
+// }
+
+
+
+// Worker = { 
+//     workplace: 'company',
+//     salary: 400,
+// }
+
+// Worker.prototype = new Human();
+// Worker.prototype.getToWork = function(){
+//     alert('Working Hard');
+// }
+
+
+// function Student() {
+
+// }
+// // Student = {
+// //     name: 'Boris',
+// //     age: 18,
+// //     male: 'male',
+// //     weight: 60,
+// //     tall: 170,
+// //     scholarship: 100,
+// //     univercity: 'КПИ'
+// // }
+
+
+// Student.prototype = new Human();
+// Student.prototype.watchSerials = function(){
+//     alert('Watching Serials All Day!')
+// }
+
+
+
+// // Worker.getToWork();
+// // Student.watchSerials();
+// console.log(Student.scholarship)
+// console.log(Worker.tall)
+// console.log(Worker.getToWork)
+// console.log()
