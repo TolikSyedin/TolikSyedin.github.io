4ad6d8251bd48b6b3099319e478b4261e042ef30
TolikSyedin <toliksyedin@gmail.com>
2016-07-01 20:54:28 +0300

    added hw19-20

--- homework/js/hw19-20/gruntfile.js
+++ homework/js/hw19-20/gruntfile.js
@@ -27,23 +27,22 @@ grunt.initConfig({
           }
         }
     },
-    cssmin: {
-      target: {
-        files: [{
-          expand: true,
-          src:  'build/css/style.main.css',
-          dest: '',
-          ext: '.min.css'
-        }]
-      }
-    },
-
-    uglify: {
-      build: {
-        src: ['build/js/script.main.js'],
-        dest: 'build/js/script.min.js'
-      }
-    },
+  cssmin: {
+    target: {
+      files: [{
+        expand: true,
+        src:  'build/css/style.main.css',
+        dest: '',
+        ext: '.min.css'
+      }]
+    }
+  },
+  uglify: {
+    build: {
+      src: ['build/js/script.main.js'],
+      dest: 'build/js/script.min.js'
+    }
+  },
 	copy: {
 		files: {
 			expand: true,
@@ -52,22 +51,49 @@ grunt.initConfig({
 			dest: 'build/'
 		}
 	},
+  processhtml: {
+    dist: {
+     files: {
+       'build/index.html': ['src/index.html']
+      }
+    }
+  },
+  htmlmin: {
+    dist: { 
+      options: {                             
+        removeComments: true,
+        collapseWhitespace: true
+      },      
+      files: {
+        'build/index.html': 'build/index.html',     
+      }
+    }
+  },
 
   clean: {
     folder: ['.sass-cache']
+  },
+
+  watch: {
+    scripts:{
+      files: ['src/styles/*', 'src/css/*', 'src/js/*', 'src/index.html',],
+      tasks: ['sass', 'concat', 'cssmin', 'uglify', 'processhtml', 'htmlmin', 'clean']
+      
+    }
   }
-
-
 });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');  
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-processhtml');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
 
 
-  grunt.registerTask('default', [ 'sass', 'concat', 'cssmin', 'uglify', 'copy', 'clean']);
+  grunt.registerTask('default', [ 'sass', 'concat', 'cssmin', 'uglify', 'copy', 'processhtml', 'htmlmin', 'clean']);
 }
