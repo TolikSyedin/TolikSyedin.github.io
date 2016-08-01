214f7d3a3bcffe732b7b6820f9fff0273eb076b4
TolikSyedin <toliksyedin@gmail.com>
2016-08-01 15:53:44 +0300

    rename repo

--- psd/mountain/gulpfile.js
+++ psd/mountain/gulpfile.js
@@ -3,55 +3,31 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
-var concatCss = require('gulp-concat-css')
-var cssmin = require('gulp-cssmin');
-var concat = require('gulp-concat');
-var uglifyjs = require('gulp-uglifyjs');
-var htmlreplace = require('gulp-html-replace');
+var svgSprite = require('gulp-svg-sprite');
+
 
  
-// gulp.task('copyfiles', function(){
-// 	gulp.src('src/css/ie8.css')
-// 		.pipe(gulp.dest('build/css'));
-// 		gulp.src('src/fonts/*.*')
-// 		.pipe(gulp.dest('build/fonts'));
-// 		gulp.src('src/img/*.*')
-// 		.pipe(gulp.dest('build/img/'));
-// 		gulp.src('src/css/ie8.css')
-// 		.pipe(gulp.dest('build/css'));
-// 		gulp.src('src/css/pie.htc')
-// 		.pipe(gulp.dest('build/css'));		
-// 		gulp.src('src/js/ie8.js')
-// 		.pipe(gulp.dest('build/js'));
-// })
+// Basic configuration example 
+var config = {
+    shape               : {
+        dimension       : {         // Set maximum dimensions 
+            maxWidth    : 35,
+            maxHeight   : 35
+        },
+    },
+    mode                : {
+        view            : {         // Activate the «view» mode 
+            bust        : false,
+        },
+    }
+};
 
-// gulp.task('styles', function(){
-// 	gulp.src('src/styles/styles.scss')
-// 	    .pipe(sass().on('error', sass.logError))
-//     	.pipe(rename('style.css'))
-//     	.pipe(gulp.dest('src/css/'));
-// 	gulp.src(['!src/css/ie8.css', 'src/css/*.css'])
-// 		.pipe(concatCss('main.css'))
-// 		.pipe(cssmin())
-// 		.pipe(rename({suffix: '.min'}))
-// 		.pipe(gulp.dest('build/css'));
-// });
- 
-// gulp.task('scripts', function() {
-// 	return gulp.src('src/js/*.js')
-// 		.pipe(concat('main.min.js'))
-// 		.pipe(uglifyjs())
-// 		.pipe(gulp.dest('build/js/'));
-// });
+gulp.task('sprite', function(){
+	gulp.src('**/*.svg', {cwd: 'src/img/svg'})
+	    .pipe(svgSprite(config))
+	    .pipe(gulp.dest('src/svg'));
+});
 
-// gulp.task('htmledit', function(){
-// 	return gulp.src('src/index.html')
-//     	.pipe(htmlreplace({
-//         	'css': 'css/main.min.css',
-//         	'js': 'js/main.min.js'
-//     	}))
-//     	.pipe(gulp.dest('build/'));
-// });
 gulp.task('sass', function () {
   return gulp.src('src/styles/styles.scss')
     .pipe(sass().on('error', sass.logError))
@@ -62,9 +38,7 @@ gulp.task('sass', function () {
 gulp.task('sass:watch', function () {
   gulp.watch('src/styles/styles.scss', ['sass']);
 });
-// gulp.task('copy', ['copyfiles']);
-// gulp.task('style', ['styles']);
-// gulp.task('script', ['scripts']);
-// gulp.task('html', ['htmledit'])
-// gulp.task('default', ['copy', 'style', 'script', 'html']);
+
+
+
 gulp.task('default', ['sass', 'sass:watch']);
