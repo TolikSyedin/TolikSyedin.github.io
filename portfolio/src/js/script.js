8400e2c22fb13744dba4a69cba73c704780380be
TolikSyedin <toliksyedin@gmail.com>
2016-08-09 15:11:57 +0300

    parallax test

--- portfolio/src/js/script.js
+++ portfolio/src/js/script.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
-	var parallaxContainer = $('.parallaxContainer');
+
 	var documentEl = $(document);
 	var body = $('html, body');
 	var wind = $(window);
+	var prev = '<svg class="svg__icon" viewBox="0 0 270.46 512"><path class="a" d="M124.94,266.21L366.47,507.73A14.49,14.49,0,0,0,387,487.24L155.74,256,387,24.79A14.54,14.54,0,0,0,376.77,0a14.13,14.13,0,0,0-10.19,4.29L125,245.82A14.41,14.41,0,0,0,124.94,266.21Z" transform="translate(-120.77 0)"/></svg>';
+	var next = '<svg class="svg__icon" viewBox="0 0 270.46 512"><path class="a" d="M387,245.82L145.42,4.29A14.13,14.13,0,0,0,135.23,0,14.54,14.54,0,0,0,125,24.79L356.26,256,125,487.24a14.49,14.49,0,0,0,20.49,20.49L387.06,266.21A14.41,14.41,0,0,0,387,245.82Z" transform="translate(-120.77 0)"/></svg>';
+	var parallaxImg = $('.parallaxImg');
 
-	// $.stellar()
+
+
 	function goToHome() {
     	body.stop(true).animate({
     		scrollTop:0
-    	}, 1500, function() {
+    	}, 1200, function() {
 	    	callback(); // Callback is required for iOS
 		});
     	return false;
@@ -16,7 +20,7 @@ $(document).ready(function(){
     function goToPortfolio() {
     	body.stop(true).animate({
     		scrollTop:$('#portfolio').offset().top
-    	}, 1000, function() {
+    	}, 1200, function() {
 	    	callback(); // Callback is required for iOS
 		});
     	return false;
@@ -24,7 +28,7 @@ $(document).ready(function(){
     function goToAbout() { 
     	body.stop(true).animate({
     		scrollTop:$('#about').offset().top
-    	}, 1000, function() {
+    	}, 1200, function() {
 	    	callback(); // Callback is required for iOS
 		});
     	return false;
@@ -33,14 +37,14 @@ $(document).ready(function(){
     function goToContacts() {
     	body.stop(true).animate({
     		scrollTop:$('#contacts').offset().top
-    	}, 1000, function() {
+    	}, 1200, function() {
 	    	callback(); // Callback is required for iOS
 		});
     	return false;
   	};
 
 	function callback() {
-		console.log('callback')
+		console.log('menu movement callback')
 	};
 
 
@@ -57,50 +61,59 @@ $(document).ready(function(){
 		}
 	}
 
-	function parallax(){
-			var topOffset = documentEl.scrollTop();
-			yPos = -(Math.floor(topOffset/8));
-			var coords = '0 ' + yPos + 'px'
-			parallaxContainer.css({
-				'backgroundPosition':  coords});		
-	}
 
-	
-		
-    // parallaxContainer.each(function(){
-    // 	var $window = $(window);
-    //     var $bgobj = $(this); // создаем объект
-    //     $(window).scroll(function() {
-    //         var yPos = -($window.scrollTop() / $bgobj.data('speed')); // вычисляем коэффициент 
-    //         // Присваиваем значение background-position
-    //         var coords = 'center '+ yPos + 'px';
-    //         // Создаем эффект Parallax Scrolling
-    //         $bgobj.css({ backgroundPosition: coords });
-    //     });
-    // });
-
-
-
-	wind.scroll(parallax);
 	wind.scroll(navigation);
+
 	$('#home-link').click(goToHome);
 	$('.header__btn').click(goToPortfolio);
     $('#portfolio-link').click(goToPortfolio);    
     $('#about-link').click(goToAbout)
 	$('#contacts-link').click(goToContacts);
  	
-	var prev = '<svg class="svg__icon" viewBox="0 0 270.46 512"><path class="a" d="M124.94,266.21L366.47,507.73A14.49,14.49,0,0,0,387,487.24L155.74,256,387,24.79A14.54,14.54,0,0,0,376.77,0a14.13,14.13,0,0,0-10.19,4.29L125,245.82A14.41,14.41,0,0,0,124.94,266.21Z" transform="translate(-120.77 0)"/></svg>';
-	var next = '<svg class="svg__icon" viewBox="0 0 270.46 512"><path class="a" d="M387,245.82L145.42,4.29A14.13,14.13,0,0,0,135.23,0,14.54,14.54,0,0,0,125,24.79L356.26,256,125,487.24a14.49,14.49,0,0,0,20.49,20.49L387.06,266.21A14.41,14.41,0,0,0,387,245.82Z" transform="translate(-120.77 0)"/></svg>';
+
 	$('.owl-carousel').owlCarousel({
 		items: 1,
-		loop: true,
+		loop: false,
 		nav: true,
 		navText: [prev,next],
 		margin: 400,
-		autoplay: false,
-		autoplayHoverPause: true,
+		// autoplay: true,
+		// autoplayHoverPause: true,
 	});
 
+
+	wind.scroll(function(){
+		parallaxImg.css('top', (Math.floor(documentEl.scrollTop()*.1*-1) + 'px'));
+	})
+
+
+	$('#portfolio').hover(function(){
+		parallaxImg.css('transform', 'none')
+	});
+	$('#portfolio').mouseleave(function(){
+		parallaxImg.css('transform', 'translate3d(0px,0px,0px)')
+
+	});
+
+
+
+
+
+	// $('.portfolio__item').hover(function(){
+	// 	var item = $(this);
+	// 	var svg = item.find('.svg');
+	// 	var desc = item.find('.portfolio__desc');
+	// 	item.animate({'padding': '0 540px 0 0'}, 800);
+	// 	svg.animate({'opacity': '1'}, 800);
+	// 	desc.animate({
+	// 		'display': 'block',
+	// 		'padding-left': 0
+	// 	}, 800);
+
+	// }, function(){
+	// 	var item = $(this);
+	// 	item.animate({'padding': '0 260px'}, 400);
+	// });
 
 
 });
