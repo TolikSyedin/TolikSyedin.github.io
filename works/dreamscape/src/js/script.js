d42d80fa4ff319e6853e82494f9c1c77d58ae276
TolikSyedin <toliksyedin@gmail.com>
2016-09-22 11:53:22 +0300

    added new work

--- works/dreamscape/src/js/script.js
+++ works/dreamscape/src/js/script.js
@@ -1,25 +1,56 @@
 $(document).ready(function () {
-	    var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        slidesPerView: 'auto',
-        loopedSlides : 20,
-		autoHeight: true,
-        centeredSlides: true,
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        spaceBetween: 0,
-        paginationClickable: true,
-        paginationBulletRender: function (index, className) {
-            return '<span class="' + className + '">' + '0' +(index + 1) + '</span>';
-        },
-        onSlideChangeStart: function(swiper){
-			var slide = swiper.slides;
-			var index = swiper.snapIndex
-			console.log(index)
-			index++;
-			$('.projects__desc').fadeOut(100);
-			$('#desc'+ index).delay(300).fadeIn(100)
-		},
+    var pCont = $('.projects__container'),
+        pWrap = $('.projects__wrapper'),
+        pItem = $('.projects__item'),
+        ww = $(window).width(),
+        params,
+        swiper = new Swiper('.swiper-container', {
+            pagination: '.swiper-pagination',
+            slidesPerView: 'auto',
+        	autoHeight: true,
+            centeredSlides: true,
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev',
+            spaceBetween: 0,
+            paginationClickable: true, 
+            autoResize: false,
+            resizeReInit: true,
+            paginationBulletRender: function (index, className) {
+                return '<span class="' + className + '">' + '0' +(index + 1) + '</span>';
+                },
+            onSlideChangeStart: function(swiper){
+        		var slide = swiper.slides;
+        		var index = swiper.snapIndex
+        		index++;
+        		$('.projects__desc').fadeOut(100);
+        		$('#desc'+ index).delay(300).fadeIn(100);
+        	   }
+            
+    });
+    params = swiper.params;
+
+    $(window).resize(function(){
+        var ww = $(window).width()
+         if(ww < 1400 && ww > 1200){
+            pItem.css('margin', '0 8% 0 0%');
+            pWrap.css({
+                marginLeft: '-200px',
+            });
+            pCont.css('margin', '0 auto');
+            swiper = new Swiper ('.swiper-container', params)
+        } else if (ww < 1200 & ww > 769) {
+            pWrap.css('marginLeft', '-70px');
+            swiper = new Swiper ('.swiper-container', params)
+
+        } else if (ww < 769 && ww > 650) {
+            pItem.css('margin', '0 13% 0 -1px');
+            pWrap.css('marginLeft', '10%');
+            swiper = new Swiper ('.swiper-container', params)
+            
+
+        }
 
     });
-});
+    $(window).trigger("resize");
+
+ });
